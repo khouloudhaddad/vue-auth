db15bad72ee1cb697aea2382099e3a77f01354fe
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-08-02 23:46:06 +0100

    Add bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 const app = createApp(App)
 
 app.use(router)
